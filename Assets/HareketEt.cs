10789099e1859b5ac3552f2106e8d9f1f9a7a477
Samed Er <sameed35000@icloud.congit config --global user.email sameed35000@icloud.com>
2022-12-01 17:30:30 +0300

    Basic Car Race Game but Alone

---
+++ Assets/HareketEt.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HareketEt : MonoBehaviour {
+
+    public WheelCollider OnSol, OnSag, ArkaSol, ArkaSag;
+
+    public float hiz, donmehizi;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update ()
+    {
+        ArkaSol.motorTorque = hiz * Input.GetAxis("Vertical");
+        ArkaSag.motorTorque = hiz * Input.GetAxis("Vertical");
+        OnSol.steerAngle = donmehizi * Input.GetAxis("Horizontal");
+        OnSag.steerAngle = donmehizi * Input.GetAxis("Horizontal");
+    }
+}
